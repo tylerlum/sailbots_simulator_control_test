764e15e59568fa811a7be6c33e2751144e81b288
Tyler <tylergwlum@gmail.com>
2018-11-24 15:50:23 -0800

    Refactor publishing of topics

--- Sailbots_Simulator_Control_Test.m
+++ Sailbots_Simulator_Control_Test.m
@@ -1,5 +1,7 @@
 %% UBC Sailbots Test Simulator Control of Sailboat from MATLAB
 %% Author: Tyler Lum
+%% http://docs.ros.org/jade/api/gazebo_msgs/html/msg/LinkStates.html
+%% http://docs.ros.org/lunar/api/std_msgs/html/msg/Float64.html
 %% Start Date: Nov 22, 2018
 
 %% Setup ros node
@@ -19,29 +21,17 @@ while true
     end
     
     %% Get current X position
-    %% /gazebo/link_states returns a cell with 55 names, poses and speeds. 
-    %% http://docs.ros.org/jade/api/gazebo_msgs/html/msg/LinkStates.html
     current_x = gazebo_link_states_msg.Pose(base_index).Position.X;
 
-    %% Setup publishing
-    [left_thrust_pub, left_thrust_msg] = rospublisher('/left_thrust_cmd', 'std_msgs/Float32');
-    [right_thrust_pub, right_thrust_msg] = rospublisher('/right_thrust_cmd', 'std_msgs/Float32');
-    
     %% If current x is too low, keep moving forward
     if current_x < 20
         x_speed = 2;
     else
         x_speed = 0;
     end
-    
-    %% Set thrust values
-    left_thrust_msg.Data = x_speed;
-    right_thrust_msg.Data = x_speed;
 
-    %% Publish thrust values
-    send(left_thrust_pub, left_thrust_msg);
-    send(right_thrust_pub, right_thrust_msg);
-
+    %% Publish speeds
+    set_thrusts(x_speed, x_speed, 0);
 end
 
 %% Functions
@@ -61,6 +51,19 @@ function index = get_index(all_names, name)
 end
 
 function set_thrusts(left_thrust, right_thrust, lateral_thrust)
-
+    %% Setup publishing
+    [left_thrust_pub, left_thrust_msg] = rospublisher('/left_thrust_cmd', 'std_msgs/Float32');
+    [right_thrust_pub, right_thrust_msg] = rospublisher('/right_thrust_cmd', 'std_msgs/Float32');
+    [lateral_thrust_pub, lateral_thrust_msg] = rospublisher('/lateral_thrust_cmd', 'std_msgs/Float32');
+    
+    %% Set thrust values
+    left_thrust_msg.Data = left_thrust;
+    right_thrust_msg.Data = right_thrust;
+    lateral_thrust_msg.Data = lateral_thrust;
+    
+    %% Publish thrust values
+    send(left_thrust_pub, left_thrust_msg);
+    send(right_thrust_pub, right_thrust_msg);
+    send(lateral_thrust_pub, lateral_thrust_msg);
 end
     
