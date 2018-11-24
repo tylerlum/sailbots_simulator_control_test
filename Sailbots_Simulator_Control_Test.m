ff091484c815e18b035181164ce91079624d2bfb
Tyler <tylergwlum@gmail.com>
2018-11-24 15:34:38 -0800

    Refactor with useful methods

--- Sailbots_Simulator_Control_Test.m
+++ Sailbots_Simulator_Control_Test.m
@@ -10,25 +10,18 @@ base_index = -1;
 %% Move robot to position x = 20, then stop
 while true
     %% Subscribe to /gazebo/link_states
-    gazebo_link_states_sub = rossubscriber('/gazebo/link_states');
-    pause(1);
-    gazebo_link_states_msg = receive(gazebo_link_states_sub, 10);
+    gazebo_link_states_msg = get_ros_msg('/gazebo/link_states');
     
     %% Get index of wamv::base_link
     if base_index == -1
-        names = gazebo_link_states_msg.Name
-        for i = 1:numel(names)
-            if strcmp('wamv::base_link', cell2mat(names(i)))
-                base_index = i
-                break
-            end
-        end
+        names = gazebo_link_states_msg.Name;
+        base_index = get_index(names, 'wamv::base_link');
     end
     
     %% Get current X position
     %% /gazebo/link_states returns a cell with 55 names, poses and speeds. 
     %% http://docs.ros.org/jade/api/gazebo_msgs/html/msg/LinkStates.html
-    current_x = gazebo_link_states_msg.Pose(base_index).Position.X
+    current_x = gazebo_link_states_msg.Pose(base_index).Position.X;
 
     %% Setup publishing
     [left_thrust_pub, left_thrust_msg] = rospublisher('/left_thrust_cmd', 'std_msgs/Float32');
@@ -50,3 +43,24 @@ while true
     send(right_thrust_pub, right_thrust_msg);
 
 end
+
+%% Functions
+function msg = get_ros_msg(topic)
+    sub = rossubscriber(topic);
+    pause(1);
+    msg = receive(sub, 10);
+end
+
+function index = get_index(all_names, name)
+    for i = 1:numel(all_names)
+        if strcmp(name, cell2mat(all_names(i)))
+            index = i;
+            break;
+        end
+    end
+end
+
+function set_thrusts(left_thrust, right_thrust, lateral_thrust)
+
+end
+    
