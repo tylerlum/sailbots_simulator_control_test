ddfc43040e377efe874171e88b02b8a0b7aa139e
Tyler <tylergwlum@gmail.com>
2018-11-24 14:52:35 -0800

    Move sailboat forward until reaches target destination, then stop

--- Sailbots_Simulator_Control_Test.m
+++ Sailbots_Simulator_Control_Test.m
@@ -3,18 +3,37 @@
 %% Start Date: Nov 22, 2018
 
 %% Setup ros node
-rosinit('localhost');
+%rosinit('localhost');
 
+%% Move robot to position x = 20, then stop
 while true
+    %% Subscribe to /gazebo/link_states
+    gazebo_link_states_sub = rossubscriber('/gazebo/link_states');
+    pause(1);
+    gazebo_link_states_msg = receive(gazebo_link_states_sub, 10);
+    
+    %% Get current X position
+    %% /gazebo/link_states returns a cell with 55 names, poses and speeds. Index 52 is the base of boat
+    %% http://docs.ros.org/jade/api/gazebo_msgs/html/msg/LinkStates.html
+    current_x = gazebo_link_states_msg.Pose(52).Position.X
+
     %% Setup publishing
     [left_thrust_pub, left_thrust_msg] = rospublisher('/left_thrust_cmd', 'std_msgs/Float32');
     [right_thrust_pub, right_thrust_msg] = rospublisher('/right_thrust_cmd', 'std_msgs/Float32');
     
-    %% Set thrust values
-    left_thrust_msg.Data = -1;
-    right_thrust_msg.Data = -1;
+    %% If current x is too low, keep moving forward
+    if current_x < 20
+        x_speed = 2;
+    else
+        x_speed = 0;
+    end
     
+    %% Set thrust values
+    left_thrust_msg.Data = x_speed;
+    right_thrust_msg.Data = x_speed;
+
     %% Publish thrust values
     send(left_thrust_pub, left_thrust_msg);
     send(right_thrust_pub, right_thrust_msg);
+
 end
